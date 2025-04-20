229fc632c541a20287d3e3129211a5cd3db75c1f
schatzsuche <geheimeschatzsuche@gmail.com>
2025-04-20 18:51:14 +0100

    Update SimpleShutdownTimer.bat

--- SimpleShutdownTimer.bat
+++ SimpleShutdownTimer.bat
@@ -1,9 +1,11 @@
 @echo off
-
-color 6	
+title Simple Shutdown Timer
+color 6
 SET /p timee= Aloha, you need to type the time in seconds inside of the console 
 ECHO  -	
 ECHO %timee%
+cls
+color 4
 ECHO your System will shutdown in %timee% seconds
 shutdown /s /t %timee%
 
