bc17ea0ef4568dbb071ff395354efc1abe2e1a36
schatzsuche <geheimeschatzsuche@gmail.com>
2025-05-12 21:47:02 +0200

    Changed it from seconds to minutes
    Changed it from seconds to minutes and also a bit of the UI

--- SimpleShutdownTimer.bat
+++ SimpleShutdownTimer.bat
@@ -1,12 +1,17 @@
 @echo off
 title Simple Shutdown Timer
 color 6
-SET /p timee= Aloha, you need to type the time in seconds inside of the console 
+ECHO.
+ECHO			 Hey, you need to type the time in minutes inside of the console 
+SET /p timee=.						
 ECHO  -	
 ECHO %timee%
+set /a timeInMin=timee * 60
 cls
 color 4
-ECHO your System will shutdown in %timee% seconds
-shutdown /s /t %timee%
+ECHO 		your System will shutdown in %timee% Min
+ECHO.
+shutdown /s /t %timeInMin%
+ECHO.
 
 PAUSE
